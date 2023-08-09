9f5be4f296187f7e9176c785c11f966de2546451
duenpu <lane715@163.com>
2023-08-09 22:59:36 +0800

    PrintBinary

---
+++ src/main/java/com/lane/interview/algorithm/PrintBinary.java
@@ -0,0 +1,18 @@
+package com.lane.interview.algorithm;
+
+public class PrintBinary {
+
+        public static void print(int num) {
+            for (int i = 31; i >= 0; i--) {
+                System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+            }
+            System.out.println();
+        }
+
+        public static void main(String[] args) {
+            //打印1-100的二进制
+            for (int i = 1; i <= 100; i++) {
+                print(i);
+            }
+        }
+}
