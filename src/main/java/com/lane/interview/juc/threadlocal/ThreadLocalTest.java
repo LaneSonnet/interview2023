854104006865b35acd0d5263dc186c9ded4ddd6a
duenpu <duenpu>
2023-08-11 16:52:49 +0800

    thread

---
+++ src/main/java/com/lane/interview/juc/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,39 @@
+package com.lane.interview.juc.threadlocal;
+
+/**
+ * @author duenpu
+ * @date 2023/8/10 20:40
+ */
+public class ThreadLocalTest {
+    private static ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                for (int i = 0;i < 100;i++) {
+                    threadLocal.set(i);
+                    System.out.println(Thread.currentThread().getName() + " threadLocal.get() = " + threadLocal.get());
+                    Thread.sleep(200);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                threadLocal.remove();
+            }
+        }, "Thread-A").start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0;i < 100;i++) {
+                    //threadLocal.set(i);
+                    System.out.println(Thread.currentThread().getName() + " threadLocal.get() = " + threadLocal.get());
+                    Thread.sleep(200);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                threadLocal.remove();
+            }
+        }, "Thread-B").start();
+    }
+}
