b7ed2bf058ab7540c9d46c89ca476578ada266f6
duenpu <lane715@163.com>
2024-03-12 22:45:03 +0800

    redrum

--- src/main/java/com/lane/interview/algorithm_workbook/p18_leetcode高频题/Q12_最大子数组和.java
+++ src/main/java/com/lane/interview/algorithm_workbook/p18_leetcode高频题/Q12_最大子数组和.java
@@ -21,60 +21,7 @@ public class Q12_最大子数组和 {
         }
     }
 
-    // 最大重复子串
-    // https://leetcode-cn.com/problems/maximum-repeating-substring/
-    public int maxRepeating(String sequence, String word) {
-        int ans = 0;
-        int n = sequence.length();
-        int m = word.length();
-        for (int i = 0; i + m <= n; i++) {
-            int j = i;
-            int k = 0;
-            while (j < n && sequence.charAt(j) == word.charAt(k)) {
-                j++;
-                k++;
-                if (k == m) {
-                    ans++;
-                    k = 0;
-                }
-            }
-        }
-        return ans;
-    }
-
     //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
